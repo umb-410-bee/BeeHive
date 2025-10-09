1c9692d06fd03cca335a81cf84fc93c3eaa5f6a7
StarshineCm <jonathanresponilla@gmail.com>
2025-10-09 16:18:01 -0400

    Refined Bee Quiz, Added Link in index.html, Removed Quiz from index.html (Again)

--- src/beeQuiz/quiz.js
+++ src/beeQuiz/quiz.js
@@ -6,12 +6,16 @@ const beeInputNo = document.querySelector("#bee-quiz-input-no");
 const beeDisplayYes = document.querySelector("#bee-quiz-display-yes");
 const beeDisplayNo = document.querySelector("#bee-quiz-display-no");
 
+const background = document.querySelector("#background");
+
 beeInputYes.addEventListener('click', function() {
     beePrompt.style.display = "none";
     beeDisplayYes.style.display = "block";
+    background.style.animation = "background-pan linear infinite 0.5s";
 })
 
 beeInputNo.addEventListener('click', function() {
     beePrompt.style.display = "none";
     beeDisplayNo.style.display = "block";
+    background.style.animation = "background-pan linear infinite 0s";
 })
